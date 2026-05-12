3794616529986312f46476841626f3c95defa277
Anna Ek <anna.ek2@outlook.com>
2026-05-12 22:52:49 +0200

    feat: add SVG assets for React and Vite, and set up initial project structure with TypeScript and Vite configuration
    - Added react.svg and vite.svg to the assets directory.
    - Created index.css for global styles with light and dark mode support.
    - Implemented main.tsx as the entry point for the React application.
    - Configured TypeScript with tsconfig.app.json and tsconfig.node.json for application and Node settings.
    - Set up vite.config.ts for Vite with React plugin.

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import axios from 'axios';
+//test fetch
+// function App() {
+// 	const fetchUsers = async () => {
+// 		try {
+// 			const response = await axios.get('https://6a035fe72afe8349b4b5252a.mockapi.io/api/users');
+// 			const users = response.data;
+// 			users.forEach((user: any) => {
+// 				user.complaints.forEach((complaint: any) => {
+// 					console.log(complaint);
+// 				});
+// 			});
+// 		} catch (error) {
+// 			console.log('could not find users', error);
+// 		}
+// 	};
+// 	fetchUsers();
+	return <></>;
+}
+
+export default App;
